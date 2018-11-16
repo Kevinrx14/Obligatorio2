217aea4581f0c1d0776334507b3bb6d0c1148805
Kevinrx14 <kevinrx14@gmail.com>
2018-11-16 19:00:31 -0300

    Agregado ingreso de texto. Closes #19

--- src/obligatorio2/Auxiliar.java
+++ src/obligatorio2/Auxiliar.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Kevin Rinaldi (240179)
+//Ex compañero Andres Martinez (184534)
 package obligatorio2;
+import java.util.*;
 
-/**
- *
- * @author kevin
- */
 public class Auxiliar {
+    //Metodo para ingresar texto
+    public String ingresarTexto() {
+        Scanner input = new Scanner(System.in);
+        String datoIngresado;
+        boolean verificador;
+
+        do {
+            verificador = true;
+
+            datoIngresado = input.nextLine();
+            //Chequeo que el texto ingresado no sea vacio
+            if (datoIngresado.isEmpty()) {
+                System.out.println("Por favor ingrese un texto");
+                verificador = false;
+            }
+        } while (verificador == false);
+
+        return datoIngresado;
+    }
     
 }
