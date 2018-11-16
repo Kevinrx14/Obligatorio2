8d5375c7d0a45768211ae1efca59770fb3800e4b
Kevinrx14 <kevinrx14@gmail.com>
2018-11-16 17:57:37 -0300

    Clase Cliente creada. Closes #12

---
+++ src/prueba/Cliente.java
@@ -0,0 +1,57 @@
+//Kevin Rinaldi (240179)
+//Ex compañero Andres Martinez (184534)
+package prueba;
+
+public class Cliente {
+
+    private String nombre;
+    private String direccion;
+    private int barrio;
+
+    //Constructor de cliente
+    public Cliente() {
+        this.setNombre(nombre);
+        this.setDireccion(direccion);
+        this.setBarrio(barrio);
+    }
+    public Cliente(String unNombre, String unaDireccion, int unBarrio) {
+        this.setNombre(unNombre);
+        this.setDireccion(unaDireccion);
+        this.setBarrio(unBarrio);
+    }
+
+    Cliente(String nombreMensajero, String direccionMensajero, String ciMensajero) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String unNombre) {
+        nombre = unNombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String unaDireccion) {
+        direccion = unaDireccion;
+    }
+
+    public int getBarrio() {
+        return barrio;
+    }
+
+    public void setBarrio(int unBarrio) {
+        barrio = unBarrio;
+    }
+
+    @Override
+    public String toString() {
+        return "Nombre: " + this.getNombre()
+                + "\n" + "Direccion: " + this.getDireccion()
+                + "\n" + "Numero de barrio: " + this.getBarrio();
+    }
+}
